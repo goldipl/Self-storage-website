7f65373df909ed8b3d069e68c5a4e1d0ab18d3fd
goldipl <mgodlewski92@gmail.com>
2023-04-15 16:25:37 +0200

    create and style Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Navbar>
+      <NavbarContainer>
+        <Logo>
+            <Link to={`#home`}>SELF<span>STORAGE</span></Link>
+        </Logo>
+        <List>
+            <Li>
+                <Link to={`#home`}>Strona główna</Link>
+            </Li>
+            <Li>
+                <Link to={`#about`}>O firmie</Link>
+            </Li>
+            <Li>
+                <Link to={`#article`}>Galeria</Link>
+            </Li>
+            <Li>
+                <Link to={`#gallery`}>Cennik</Link>
+            </Li>
+        </List>
+      </NavbarContainer>
+    </Navbar>
   )
 }
+
+export const Navbar = styled.div `
+    width: 100vw;
+    height: 60px;
+    background-color: #FFFFFF;
+    box-shadow: 0 4px 8px rgba(244, 11, 0, 0.15);
+`;
+
+export const NavbarContainer = styled.div `
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 70vw;
+    height: 60px;
+    margin: 0 auto;
+`;
+
+export const Logo = styled.div `
+    font-size: 24px;
+    font-weight: 600;
+    color: #000000;
+    margin-right: 16px;
+    a {
+        text-decoration: none;
+        color: #000000;
+        span {
+          font-size: 12px;
+          font-weight: 400;
+          color: #ED0F04;
+        }
+    }
+`;
+
+export const List = styled.ul `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+`;
+
+export const Li = styled.li `
+    margin-right: 16px;
+    list-style: none;
+    a {
+        display: block;
+        margin: 16px;
+        width: max-content;
+        text-decoration: none;
+        color: #ED0F04;
+        &:hover {
+            font-weight: 600;
+        }
+    }
+`;
 
 export default Header
