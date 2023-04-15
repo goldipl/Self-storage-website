e77d1c52f7dd6e22c6cb8a0663c2827a56ff3f1b
goldipl <mgodlewski92@gmail.com>
2023-04-15 17:19:28 +0200

    styling Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <HeroContainer></HeroContainer>
   )
 }
+
+export const HeroContainer = styled.div `
+  width: 100%;
+  height: 600px;
+  background-color: #043A8F;
+`;
 
 export default Hero
