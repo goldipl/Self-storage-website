c7331e7b166cabd9b04bfa6304019dc1f19d75be
goldipl <mgodlewski92@gmail.com>
2023-04-16 12:03:15 +0200

    styling footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <FooterContainer>
+      <Text>Footer</Text>
+    </FooterContainer>
   )
 }
+
+const FooterContainer = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Text = styled.p `
+  font-size: 16px;
+`
 
 export default Footer
